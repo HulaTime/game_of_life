7c8163507a1b410094b19381123d9c3607bcbc4a
MaxProvin <maxprovin01@gmail.com>
2016-11-23 14:18:33 +0000

    Cell state is dead by default

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,4 +2,12 @@ require 'cell'
 
 describe Cell do
 	
+	subject(:cell) { described_class.new }
+	
+	it { is_expected.to respond_to(:alive) }
+
+	it 'Status defaults to being dead' do
+		expect(cell.alive).to be false
+	end
+
 end
--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -5,17 +5,20 @@ describe World do
 	GRID_SIZE = 3
 
 	subject(:world) { described_class.new(GRID_SIZE,GRID_SIZE) }
+	let(:cell_obj) { double :cell_obj }
 
 	it { is_expected.to respond_to(:row_num, :col_num, :grid) }
 
-	it 'Number of rows and columns is equal to initialized arguments' do
-		expect(world.col_num).to eq GRID_SIZE
-		expect(world.row_num). to eq GRID_SIZE
-	end
+	context 'Grid' do
+		it 'Number of rows and columns is equal to initialized arguments' do
+			expect(world.col_num).to eq GRID_SIZE
+			expect(world.row_num). to eq GRID_SIZE
+		end
 
-	it 'Grid generated with supplied no. of columns and rows' do
-		expect(world.grid[0].length).to eq GRID_SIZE
-		expect(world.grid[1].length).to eq GRID_SIZE
+		it 'Generates 2D array with supplied no. of columns and rows' do
+			expect(world.grid.is_a?(Array)).to be true
+			expect(world.grid[0].length).to eq GRID_SIZE
+			expect(world.grid[1].length).to eq GRID_SIZE
+		end
 	end
-
 end
