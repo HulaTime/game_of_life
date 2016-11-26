fa787790fd837489e7fcf35aa569108376c9265d
MaxProvin <maxprovin01@gmail.com>
2016-11-26 22:33:34 +0000

    Game class is initialized with a world and cell object

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,17 @@ require 'game'
 
 describe Game do
 
-	subject(:game) { described_class.new }
+	subject(:game) { described_class.new(world, cell_obj) }
+
+	let(:world) { double :world }
+	let(:cell_obj) { double :cell_obj }
+
+	it { is_expected.to respond_to(:world, :cell_obj) }
+
+	it 'Initialized with a world and cell object to populate the world' do
+		expect(game.world).to eq world
+		expect(game.cell_obj).to eq cell_obj
+	end
 
 	context 'Rules:' do
 		xcontext 'Rule 1:' do
@@ -19,6 +29,7 @@ describe Game do
 		end
 		xcontext 'Rule 4:' do
 			it 'Dead cells with exactly 3 live neighbours becomes a live cell' do
+
 			end
 		end
 	end
