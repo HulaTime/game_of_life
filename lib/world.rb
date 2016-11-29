4773bdfb8f4c5b22b34c784d7890edfa4265e55e
MaxProvin <maxprovin01@gmail.com>
2016-11-29 19:13:06 +0000

    world grid is initialized with a cell for each position on the grid

--- lib/world.rb
+++ lib/world.rb
@@ -2,12 +2,10 @@ class World
 
   attr_reader :row_num, :col_num, :grid
 
-  def initialize(rows, columns, cell_obj = Cell.new)
-    @row_num = rows
-    @col_num = columns
-    @grid = Array.new(col_num) do
-    					Array.new(row_num) do
-    						cell_obj
+  def initialize(cell = Cell.new)
+    @grid = Array.new(3) do
+    					Array.new(3) do
+    					  cell
     					end
     				end
   end
--- spec/units/world_spec.rb
+++ spec/units/world_spec.rb
@@ -2,6 +2,24 @@ require 'world'
 
 describe World do
 
-  subject(:world) { described_class.new }
+  subject(:world) { described_class.new(cell_obj) }
+
+  let(:grid) { Array.new(3) { Array.new(3) { cell_obj } } }
+  let(:cell_obj) { double :cell_obj }
+
+  context 'Grid' do
+    it 'is a 2D array' do
+      expect(world.grid).to be_an Array
+      expect(world.grid[0]).to be_an Array
+    end
+
+    it 'Cells are external objects introduced in initiation' do
+      world.grid.each do |column|
+        column.each do |cell|
+          expect(cell).to eq cell_obj
+        end
+      end
+    end
+  end
 
 end
